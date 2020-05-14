6b1cafce226c6be3f46d768d12eb3d6e325b813e
Dhemes Mota <dhemes.mota@gmail.com>
2020-05-14 11:13:14 -0300

    Adicionando variaveis de ambiente

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '4a292cc65d2d3e2559a46902001ba202',
-    expiresIn: '1d',
+    secret: process.env.APP_SECRET || '4a292cc65d2d3e2559a46902001ba202',
+    expiresIn: process.env.APP_EXPIRES_IN || '1d',
   },
 };
