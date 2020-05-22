b973859dbd2a9bbacda5463adc3986dba634bb77
Dhemes Mota <dhemes.mota@gmail.com>
2020-05-22 10:07:11 -0300

    Configurando o header do dashboard

--- gobarber-web/src/pages/Dashboard/index.tsx
+++ gobarber-web/src/pages/Dashboard/index.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
 
+import logoImg from '../../assets/logo.svg';
+import userAvatarDefault from '../../assets/user-avatar.png';
 import { useAuth } from '../../hooks/auth';
 import useCan from '../../hooks/useCan';
-import { Container } from './styles';
+import { Container, Header, HeaderContent, Profile } from './styles';
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
   const can = useCan();
 
   return (
     <Container>
-      <h1>Dashboard</h1>
-      {can('has_logout') && (
-        <button onClick={() => signOut()} type="button">
-          Sair do sistema
-        </button>
-      )}
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url || userAvatarDefault} alt="Dhemes Mota" />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          {!can('has_logout') && (
+            <button onClick={() => signOut()} type="button">
+              <FiPower />
+            </button>
+          )}
+        </HeaderContent>
+      </Header>
     </Container>
   );
 };
