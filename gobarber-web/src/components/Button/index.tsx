c4ea4106b30cb830369e1bb3efcc3d1693316b2d
Dhemes Mota <dhemes.mota@gmail.com>
2020-05-22 07:58:21 -0300

    Criando pagina de recuperação de senha

--- gobarber-web/src/components/Button/index.tsx
+++ gobarber-web/src/components/Button/index.tsx
@@ -2,12 +2,14 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { BtnContainer } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => {
   return (
     <BtnContainer type="button" {...rest}>
-      {children}
+      {loading ? 'Carregando...' : children}
     </BtnContainer>
   );
 };
