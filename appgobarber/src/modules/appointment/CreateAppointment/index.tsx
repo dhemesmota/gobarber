6d6236814bb2c963c1802326b6190d43c8c2b88b
Dhemes Mota <dhemes.mota@gmail.com>
2020-06-11 10:42:45 -0300

    Adicionando rotas de agendamentos

---
+++ appgobarber/src/modules/appointment/CreateAppointment/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CreateAppointment: React.FC = () => {
+  return <Container>Profile</Container>;
+};
+
+export default CreateAppointment;
--- appgobarber/src/routes/app.routes.tsx
+++ appgobarber/src/routes/app.routes.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
+import AppointmentCreated from '../modules/appointment/AppointmentCreated';
+import CreateAppointment from '../modules/appointment/CreateAppointment';
 import Dashboard from '../modules/dashboard/pages/Dashboard';
+import Profile from '../modules/user/pages/Profile';
 
 const App = createStackNavigator();
 
@@ -13,7 +16,12 @@ const AppRoutes: React.FC = () => (
       cardStyle: { backgroundColor: '#312e38' },
     }}
   >
+    {/* Rotas de agendamento */}
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
